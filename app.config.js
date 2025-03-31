56cd9e832d4aa5b5bfb461d6f19b7c56fd08bda6
420btc <chocolati_558@hotmail.com>
2025-03-31 02:12:54 +0200

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "Meteo Málaga",
+  name: "Meteo Malaga",
   slug: "meteo-malaga",
   version: "1.0.0",
   orientation: "portrait",
